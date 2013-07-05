8f9d0c94efeee396da3f8ac2369886b68a5f9f78
Frank McAuley <frank.mcauley@gmail.com>
2013-07-05 11:25:11 -0400

    Added NSCache
    Added an NSCache instance to the UICollectionView to hold the imageURL and the indexPath row reference.
    
    Signed-off-by: Frank McAuley <frank.mcauley@gmail.com>

--- DribbbleClient/FMSDribbbleCollectionViewViewController.m
+++ DribbbleClient/FMSDribbbleCollectionViewViewController.m
@@ -8,8 +8,11 @@
 
 #import "FMSDribbbleCollectionViewViewController.h"
 #import "FMSFetchDribbbleData.h"
+#import "FMSDribbbleImageCell.h"
 
 @interface FMSDribbbleCollectionViewViewController ()
+
+@property (nonatomic, strong) NSCache *imageCache;
 @end
 
 @implementation FMSDribbbleCollectionViewViewController
@@ -17,13 +20,43 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
 	[FMSFetchDribbbleData fetchDribbbleData];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    self.imageCache = nil;
+}
+
+#pragma mark UICollectionViewController Delegate Methods
+
+
+#pragma mark UICollectionViewController DataSource Methods
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 100;
+}
+
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    FMSDribbbleImageCell* newCell = [self.collectionView dequeueReusableCellWithReuseIdentifier:@"Cell"
+                                                                           forIndexPath:indexPath];
+    //Configure the cell
+    NSURL *imageURL = [NSURL URLWithString:@"http://dribbble.s3.amazonaws.com/users/339875/screenshots/1144149/creativity_teaser.png"];
+    NSData *imageData = [NSData dataWithContentsOfURL:imageURL]; // Kick off on a background and the place holder
+    //also only load when the scrolling has stopped
+    
+    UIImage *image = [UIImage imageWithData:imageData];
+    
+    // Setup NSCache for the image url and index.row position
+   
+    
+    newCell.dribbbleImage.image = image;
+    
+    return newCell;
 }
 
 @end
