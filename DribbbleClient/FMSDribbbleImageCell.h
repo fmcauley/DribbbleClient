090fa295d917852329e8eec1f6f009600353f8ef
Frank McAuley <frank.mcauley@gmail.com>
2013-07-05 11:22:08 -0400

    Custom Cell
    Added a Custom Cell of the UICollectionView of Images.
    
    Signed-off-by: Frank McAuley <frank.mcauley@gmail.com>

---
+++ DribbbleClient/FMSDribbbleImageCell.h
@@ -0,0 +1,13 @@
+//
+//  FMSDribbbleImageCell.h
+//  DribbbleClient
+//
+//  Created by Frank McAuley on 7/5/13.
+//  Copyright (c) 2013 Frank McAuley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMSDribbbleImageCell : UICollectionViewCell
+
+@end
---
+++ DribbbleClient/FMSDribbbleImageCell.m
@@ -0,0 +1,31 @@
+//
+//  FMSDribbbleImageCell.m
+//  DribbbleClient
+//
+//  Created by Frank McAuley on 7/5/13.
+//  Copyright (c) 2013 Frank McAuley. All rights reserved.
+//
+
+#import "FMSDribbbleImageCell.h"
+
+@implementation FMSDribbbleImageCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
