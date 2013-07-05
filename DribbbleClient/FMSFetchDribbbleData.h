cb65b38bcae15e4c43937e38038f7a2cf68ccc46
Frank McAuley <frank.mcauley@gmail.com>
2013-07-05 10:32:49 -0400

    Added the FetchDribble Class
    Added the Fetch Dribbble Data class. This is refactoring of the fetch data logic from the View Controller.
    
    Signed-off-by: Frank McAuley <frank.mcauley@gmail.com>

---
+++ DribbbleClient/FMSFetchDribbbleData.h
@@ -0,0 +1,15 @@
+//
+//  FMSFetchDribbbleData.h
+//  DribbbleClient
+//
+//  Created by Frank McAuley on 7/5/13.
+//  Copyright (c) 2013 Frank McAuley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FMSFetchDribbbleData : NSObject
+
++ (void)fetchDribbbleData;
+
+@end
---
+++ DribbbleClient/FMSFetchDribbbleData.m
@@ -0,0 +1,55 @@
+//
+//  FMSFetchDribbbleData.m
+//  DribbbleClient
+//
+//  Created by Frank McAuley on 7/5/13.
+//  Copyright (c) 2013 Frank McAuley. All rights reserved.
+//
+
+#define kDribbbleBGQueue dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+#define kDribbbleURLEveryoneURL [NSURL URLWithString:@"http://api.dribbble.com/shots/everyone"]
+
+#import "FMSFetchDribbbleData.h"
+
+@interface FMSFetchDribbbleData ()
+
+- (void)fetchInitalDataFromDribbble;
+- (void)parseDribbbleData:(NSData *)responseData;
+
+@end
+
+@implementation FMSFetchDribbbleData
+
++ (void)fetchDribbbleData
+{
+    FMSFetchDribbbleData *local = [[FMSFetchDribbbleData alloc]init];
+    [local fetchInitalDataFromDribbble];
+    
+}
+
+- (void)fetchInitalDataFromDribbble
+{
+    dispatch_async(kDribbbleBGQueue, ^{
+        NSData* data = [NSData dataWithContentsOfURL:
+                        kDribbbleURLEveryoneURL];
+        [self performSelectorOnMainThread:@selector(parseDribbbleData:)
+                               withObject:data waitUntilDone:YES];
+    });
+}
+
+- (void)parseDribbbleData:(NSData *)responseData
+{
+    NSError* error;
+    NSDictionary* json = [NSJSONSerialization
+                          JSONObjectWithData:responseData
+                          
+                          options:kNilOptions
+                          error:&error];
+    
+    NSArray* latestLoans = [json objectForKey:@"shots"];
+    
+    
+    NSLog(@"shots: %@",latestLoans);
+}
+
+@end
