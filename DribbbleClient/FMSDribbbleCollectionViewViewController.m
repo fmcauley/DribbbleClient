268b02104a5d27564fb4f0f04a1aa09bf296c7e7
Frank McAuley <frank.mcauley@gmail.com>
2013-07-05 10:19:34 -0400

    Added URL and Parse logic
    Added logic for accessing the url and parsing JSON data from the Dribbble api.
    
    Signed-off-by: Frank McAuley <frank.mcauley@gmail.com>

--- DribbbleClient/FMSDribbbleCollectionViewViewController.m
+++ DribbbleClient/FMSDribbbleCollectionViewViewController.m
@@ -6,33 +6,56 @@
 //  Copyright (c) 2013 Frank McAuley. All rights reserved.
 //
 
+#define kDribbbleBGQueue dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+#define kDribbbleURLEveryoneURL [NSURL URLWithString:@"http://api.dribbble.com/shots/everyone"]
+
+
 #import "FMSDribbbleCollectionViewViewController.h"
 
 @interface FMSDribbbleCollectionViewViewController ()
+- (void)loadDataFromDribbble;
+- (void)fetchedData:(NSData *)responseData;
 
 @end
 
 @implementation FMSDribbbleCollectionViewViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+	[self loadDataFromDribbble];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)loadDataFromDribbble
+{
+    dispatch_async(kDribbbleBGQueue, ^{
+        NSData* data = [NSData dataWithContentsOfURL:
+                        kDribbbleURLEveryoneURL];
+        [self performSelectorOnMainThread:@selector(fetchedData:)
+                               withObject:data waitUntilDone:YES];
+    });
+}
+
+- (void)fetchedData:(NSData *)responseData
+{
+    //parse out the json data
+    NSError* error;
+    NSDictionary* json = [NSJSONSerialization
+                          JSONObjectWithData:responseData
+                          
+                          options:kNilOptions
+                          error:&error];
+    
+    NSArray* latestLoans = [json objectForKey:@"shots"];
+    
+    
+    NSLog(@"shots: %@",latestLoans);
 }
 
 @end
