6c48392d3aae7037132e10ce63556288972a3066
Frank McAuley <frank.mcauley@gmail.com>
2013-07-05 10:33:48 -0400

    Refactored Call
    Refactored the call of the FMSFetchDribbbleData class to use a Class method. This gives the call a cleaning interface.
    
    Signed-off-by: Frank McAuley <frank.mcauley@gmail.com>

--- DribbbleClient/FMSDribbbleCollectionViewViewController.m
+++ DribbbleClient/FMSDribbbleCollectionViewViewController.m
@@ -6,16 +6,10 @@
 //  Copyright (c) 2013 Frank McAuley. All rights reserved.
 //
 
-#define kDribbbleBGQueue dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
-#define kDribbbleURLEveryoneURL [NSURL URLWithString:@"http://api.dribbble.com/shots/everyone"]
-
-
 #import "FMSDribbbleCollectionViewViewController.h"
+#import "FMSFetchDribbbleData.h"
 
 @interface FMSDribbbleCollectionViewViewController ()
-- (void)loadDataFromDribbble;
-- (void)fetchedData:(NSData *)responseData;
-
 @end
 
 @implementation FMSDribbbleCollectionViewViewController
@@ -23,39 +17,13 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	[self loadDataFromDribbble];
+	[FMSFetchDribbbleData fetchDribbbleData];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-- (void)loadDataFromDribbble
-{
-    dispatch_async(kDribbbleBGQueue, ^{
-        NSData* data = [NSData dataWithContentsOfURL:
-                        kDribbbleURLEveryoneURL];
-        [self performSelectorOnMainThread:@selector(fetchedData:)
-                               withObject:data waitUntilDone:YES];
-    });
-}
-
-- (void)fetchedData:(NSData *)responseData
-{
-    //parse out the json data
-    NSError* error;
-    NSDictionary* json = [NSJSONSerialization
-                          JSONObjectWithData:responseData
-                          
-                          options:kNilOptions
-                          error:&error];
-    
-    NSArray* latestLoans = [json objectForKey:@"shots"];
-    
-    
-    NSLog(@"shots: %@",latestLoans);
 }
 
 @end
