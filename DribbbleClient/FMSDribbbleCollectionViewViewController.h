0b9c9493633a9df9033ffda664145ff7c9e7e3a7
Frank McAuley <frank.mcauley@gmail.com>
2013-07-05 10:09:24 -0400

    Addition of Storyboard
    Added a storyboard to the blank client. Need to update the plist and the app delegate for the changes.
    
    Signed-off-by: Frank McAuley <frank.mcauley@gmail.com>

---
+++ DribbbleClient/FMSDribbbleCollectionViewViewController.h
@@ -0,0 +1,13 @@
+//
+//  FMSDribbbleCollectionViewViewController.h
+//  DribbbleClient
+//
+//  Created by Frank McAuley on 7/5/13.
+//  Copyright (c) 2013 Frank McAuley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMSDribbbleCollectionViewViewController : UICollectionViewController
+
+@end
---
+++ DribbbleClient/FMSDribbbleCollectionViewViewController.m
@@ -0,0 +1,38 @@
+//
+//  FMSDribbbleCollectionViewViewController.m
+//  DribbbleClient
+//
+//  Created by Frank McAuley on 7/5/13.
+//  Copyright (c) 2013 Frank McAuley. All rights reserved.
+//
+
+#import "FMSDribbbleCollectionViewViewController.h"
+
+@interface FMSDribbbleCollectionViewViewController ()
+
+@end
+
+@implementation FMSDribbbleCollectionViewViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
