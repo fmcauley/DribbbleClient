c35781b412492bf558699b7be11a56b1d8feebc1
Frank McAuley <frank.mcauley@gmail.com>
2013-07-08 15:47:20 -0400

    Added View Controller
    Added the image detail View Controller
    
    Signed-off-by: Frank McAuley <frank.mcauley@gmail.com>

---
+++ FMSImageDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FMSImageDetailsViewController.h
+//  DribbbleClient
+//
+//  Created by Frank McAuley on 7/8/13.
+//  Copyright (c) 2013 Frank McAuley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMSImageDetailsViewController : UIViewController
+
+@end
---
+++ FMSImageDetailsViewController.m
@@ -0,0 +1,38 @@
+//
+//  FMSImageDetailsViewController.m
+//  DribbbleClient
+//
+//  Created by Frank McAuley on 7/8/13.
+//  Copyright (c) 2013 Frank McAuley. All rights reserved.
+//
+
+#import "FMSImageDetailsViewController.h"
+
+@interface FMSImageDetailsViewController ()
+
+@end
+
+@implementation FMSImageDetailsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
